9fcea82cdaaf21f6656c0931ef15860f0047fdc2
MBaroky <m.baroky@gmail.com>
2021-04-19 00:58:26 +0200

    got it to work el7 finally

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,14 +4,51 @@ function handleSubmit(event) {
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
-
+    let data = {
+        "text":formText
+    }
     console.log("::: Form Submitted :::")
     fetch('http://localhost:8081/test', {
-        method: 'POST'
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify(data)
     })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        console.log(res)
+        let {
+            agreement,
+            confidence,
+            irony,
+            model,
+            score_tag,
+            subjectivity
+        } = res
+        document.getElementById('results').innerHTML =
+        `<table class="results">
+        <tr>
+        <td> agreement: </td> <td> ${agreement}</td>
+        </tr>
+        <tr>
+        <td> confidence:</td> <td>  ${confidence}</td>
+        </tr>
+        <tr>
+        <td> irony:</td> <td>  ${irony}</td>
+        </tr>
+        <tr>
+        <td> model:</td> <td>  ${model}</td>
+        </tr>
+        <tr>
+        <td> score_tag: </td> <td> ${score_tag}</td>
+        </tr>
+        <tr>
+        <td> subjectivity: </td> <td> ${subjectivity}</td>
+        </tr>
+        </table>
+        `
     })
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -2,14 +2,19 @@ const dotenv = require('dotenv')
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const cors = require('cors')
+const fetch = require('node-fetch')
+var FormData = require('form-data');
 dotenv.config();
 
 const app = express()
 
 app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
 
 console.log(__dirname)
-// console.log(`the API key is: ${process.env.API_key}`)
+const key = process.env.API_key;
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
@@ -20,7 +25,28 @@ app.get('/', function (req, res) {
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
+app.post('/test', (req,res)=>{
+    console.log(req.body.text, process.env.API_key)
+    const formdata = new FormData();
+        formdata.append("key", process.env.API_key);
+        formdata.append("txt", req.body.text);
+        formdata.append("lang", "en");  // 2-letter code, like en es fr ...
 
+    const requestOptions = {
+    method: 'POST',
+    body: formdata,
+    redirect: 'follow'
+    };
+
+
+    const response = fetch(`https://api.meaningcloud.com/sentiment-2.1`, requestOptions)
+      .then(response =>  response.json() )
+      .then((data) => {
+          console.log(data.status)
+          res.send(data)
+        })
+      .catch(error => console.log('error', error));
+})
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
