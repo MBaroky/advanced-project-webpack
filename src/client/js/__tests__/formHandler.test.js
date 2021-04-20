1d4bcc82b664c0a4f0447dbe37734c5664b71d7a
MBaroky <m.baroky@gmail.com>
2021-04-20 03:11:36 +0200

    still

--- src/client/js/__tests__/formHandler.test.js
+++ src/client/js/__tests__/formHandler.test.js
@@ -4,8 +4,10 @@ describe('handleSubmit', ()=>{
     console.log('NVM');
     expect(console.log).toHaveBeenCalledWith('NVM');
     jest.spyOn(window, 'alert').mockImplementation(() => {});
-
+    jest.mock('../formHandler.js')
+    preventDefault();
     test('passing URL to return error', ()=>{
+        expect(preventDefault).toHaveBeenCalled();
         expect(handleSubmit('https://learn.meaningcloud.com/developer/sentiment-analysis/2.1/doc/examples')).toBe('url is not allowed');
     })
 })
